3d961f98bd4b0767deab93fdb341e3ecbd2aa266
Alexsander Hage de Melo <hagemelo@gmail.com>
2020-09-30 10:26:39 -0300

    Correcoes de vulnerabilidade em nodejs

--- dispag/auth-dispag/handler.js
+++ dispag/auth-dispag/handler.js
@@ -30,6 +30,29 @@ module.exports.login = async event => {
   };
 };
 
+
+
+module.exports.loginteste = async event => {
+  
+  const data = JSON.parse(event.body);
+
+
+  console.log(data)
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'OK',
+         },
+      null,
+      2
+    ),
+  };
+};
+
+
+
 module.exports.verificartk = async event => {
   
   const token  = event.headers.token
